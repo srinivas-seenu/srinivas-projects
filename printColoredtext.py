e9daa3a789766532a4be5c2796d3be869a84d3f2
srinivas <you@example.com>
2024-11-29 17:23:18 +0530

    updated printcoloured.py

--- printColoredtext.py
+++ printColoredtext.py
@@ -4,4 +4,4 @@ colorama.init(autoreset=True)
 
 print(Fore.BLUE+Back.YELLOW+"Hi My name is Aman Kharwal "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
 print(Back.CYAN+"Hi My name is Aman Kharwal")
-print(Fore.RED + Back.GREEN+ "Hi My name is Aman Kharwal")
+print(Fore.RED + Back.GREEN+ "Hi My name is Aman Kharwal")ffghhghg ggth hhyhy 
