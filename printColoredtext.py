4908599f3c910e4c8d3ef8d397a3dbb22efb49bc
srinivas <you@example.com>
2024-11-29 17:35:15 +0530

    add second

--- printColoredtext.py
+++ printColoredtext.py
@@ -3,5 +3,5 @@ from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 print(Fore.BLUE+Back.YELLOW+"Hi My name is srinivas "+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
-print(Back.CYAN+"Hi My name is srinivas")
+print(Back.CYAN+"Hi My name is srinivas")fggvvddcd dgddgfdgdhhd hddhdvdddh
 print(Fore.RED + Back.GREEN+ "Hi My name is srinivas")
